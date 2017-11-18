bbc289445eec8161a88370e69f3649e55e307815
yuzheli <yuzheli@vip.qq.com>
2017-11-18 10:44:47 +0800

    some alias[copyright:Malloclab-cmu]

---
+++ alias.c
@@ -0,0 +1,13 @@
+#ifdef DEBUG
+/* When debugging is enabled, these form aliases to useful functions */
+#define dbg_printf(...) printf(__VA_ARGS__)
+#define dbg_requires(...) assert(__VA_ARGS__)
+#define dbg_assert(...) assert(__VA_ARGS__)
+#define dbg_ensures(...) assert(__VA_ARGS__)
+#else
+/* When debugging is disnabled, no code gets generated for these */
+#define dbg_printf(...)
+#define dbg_requires(...)
+#define dbg_assert(...)
+#define dbg_ensures(...)
+#endif
